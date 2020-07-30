45a505ff99c6d5825243a8c33938832942fa01d6
gabrielle faria <gabriellefariat@gmail.com>
2020-07-29 23:12:54 -0300

    fim aula 2

--- src/pages/cadastro/Categoria/categoriaIndex.js
+++ src/pages/cadastro/Categoria/categoriaIndex.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import PageDefault from '../../../componentes/PageDefault/pageDefaultIndex'
-
+ 
 function CadastroCategoria() {
 
     return(
         <PageDefault>
-                <h1>PAGINA DE CADASTRO DE CATEGORIA</h1>
-                <Link to="/">
-                    Home
-                </Link>
-        </PageDefault>
+        <h1>Cadastro de Categoria</h1>
+  
+        <form>
+  
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+  
+          <button>
+            Cadastrar
+          </button>
+        </form>
+  
+  
+        <Link to="/">
+          Ir para home
+        </Link>
+      </PageDefault>
     )
 }
 
